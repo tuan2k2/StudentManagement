44f80ad2c11b0c6858cfe98a256d75eb63cc0684
tuanta <tuana1k56@gmail.com>
2021-08-28 15:38:25 +0700

    lesson9

--- src/oppJava/Sentence2/Student.java
+++ src/oppJava/Sentence2/Student.java
@@ -1,4 +1,72 @@
 package oppJava.Sentence2;
 
+import java.util.Scanner;
+
 public class Student {
+    private int studentCode;
+    private String name;
+    private String major;
+    private float theoryScore;
+    private float praticeScore;
+
+    public Student(int studentCode, String name, String major, float theoryScore, float praticeScore) {
+        this.studentCode = studentCode;
+        this.name = name;
+        this.major = major;
+        this.theoryScore = theoryScore;
+        this.praticeScore = praticeScore;
+    }
+
+    public Student() {
+    }
+    public double getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode( int studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    public float getTheoryScore() {
+        return theoryScore;
+    }
+
+    public void setTheoryScore(float theoryScore) {
+        this.theoryScore = theoryScore;
+    }
+    public float getPraticeScore() {
+        return praticeScore;
+    }
+
+    public void setPraticeScore(float praticeScore) {
+        this.praticeScore = praticeScore;
+    }
+    public float calculateAverageScore(float dlt , float dth){
+        dlt = theoryScore;
+        dth = praticeScore;
+        float dtb = (dlt+dth)/2;
+        return dtb;
+    }
+    public String toString(){
+        return "[ MSV: " +  studentCode + ";" + " HỌ TÊN: " + name + ";" + " CHUYÊN NGÀNH: " + major + ";" + " DLT: " + theoryScore + ";" + " DTH: " + praticeScore  + ";" + " DTB: "  + calculateAverageScore(theoryScore,praticeScore) + " ]";
+    }
 }
+
+
+
+
